3b16338579fb1282878ff05dbf9473174a1e4830
virinarl <vrodriguezl@fi.uba.ar>
2021-05-13 23:53:02 -0300

    navbar básico

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+    return <div>
+        <h1 class="header">Iri-store</h1>
+        <p><a href="#">Inicio</a></p>
+        <p><a href="#">Categorías</a></p>
+        </div>;
+};
+
+export default NavBar
