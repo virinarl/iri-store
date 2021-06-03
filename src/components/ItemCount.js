80a8d2aba74cabb76bee38eb8f4addc4e2cea32c
virinarl <vrodriguezl@fi.uba.ar>
2021-06-03 20:19:00 -0300

    update itemCount and styles

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import {useState} from 'react';
+import './styles/itemCounts.css'
 
 const ItemCounts = ({initial , stock}) => {
     const [counter, setCounter] = useState (initial);
       
-
-    const updateCounter = (value) =>{
-                
-        if ((counter > 0 && value === -1 ) || (counter < stock && value === 1)){
-            setCounter(counter + value);
-        }
+    const addCount = () => {
+        setCounter(counter + 1);
     }
 
-    return <div>
-        <h5>Contando</h5>
-        <button onClick = {() => updateCounter(-1)}>Restar Uno</button>
-        <button onClick = {() => updateCounter(1)}>Sumar Uno</button>
-        Counter = {counter}
-    </div>;
+    const lessCount = () => {
+        setCounter(counter - 1);
+    }
+    
+    return (
+        <div className='itemCountMain'>
+            <div className='itemCountContainer'>
+                <button onClick = {lessCount} disabled={counter === 1 && 'disabled'}>-</button>
+                <span className='number'>{counter}</span>
+                <button onClick = {addCount} disabled={counter === stock && 'disabled'}>+</button>
+            </div>
+        </div>
+    )
 }
 
 export default ItemCounts
