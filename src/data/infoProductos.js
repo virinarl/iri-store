4b78a44043e1d20a1cee48a93016708a9dd9e861
virinarl <vrodriguezl@fi.uba.ar>
2021-06-12 17:17:21 -0300

    Product updated

--- src/data/infoProductos.js
+++ src/data/infoProductos.js
@@ -1,6 +1,14 @@
 import amigorumiWolf from "../assets/products/amigorumiWolf.jpg";
 import sonajeroBear from "../assets/products/bearForBabies.jpg";
 import amigorumiBear from "../assets/products/Bear.jpg";
+import llaveroBonny from "../assets/products/llaveroBonny.jpg";
+import bonniesForBabies from "../assets/products/bunniesForBabies.jpg";
+import hippo from "../assets/products/hipopotamo.jpg";
+import bonnyForBabies from "../assets/products/bonnyForBabies.jpg";
+import octopus from "../assets/products/pulpo.jpg";
+import jirafa from "../assets/products/jirafa.jpg";
+import dog from "../assets/products/dog.jpg";
+import monsterBear from "../assets/products/monsterBear.jpg";
 
 const misProductos = [
   {
@@ -11,17 +19,17 @@ const misProductos = [
     stock: 4,
     descripcion: "Lorem ipsum dolor sit amet, consectetur.",
     oferta: 0,
-    categoria: "peluche",
+    categoria: "peluches",
   },
   {
     id: "2",
-    title: "Mordillo",
+    title: "Mordillo Oso",
     price: 150,
     pictureUrl: sonajeroBear,
     stock: 7,
     descripcion: "Lorem ipsum dolor sit amet, consectetur.",
     oferta: 1,
-    categoria: "bebe",
+    categoria: "bebes",
   },
   {
     id: "3",
@@ -31,7 +39,87 @@ const misProductos = [
     stock: 2,
     descripcion: "Lorem ipsum dolor sit amet, consectetur.",
     oferta: 0,
-    categoria: "peluche",
+    categoria: "peluches",
+  },
+  {
+    id: "4",
+    title: "Llavero Conejo",
+    price: 200,
+    pictureUrl: llaveroBonny,
+    stock: 4,
+    descripcion: "Lorem ipsum dolor sit amet, consectetur.",
+    oferta: 0,
+    categoria: "accesorios",
+  },
+  {
+    id: "5",
+    title: "Set mordillo Conejo",
+    price: 500,
+    pictureUrl: bonniesForBabies,
+    stock: 3,
+    descripcion: "Lorem ipsum dolor sit amet, consectetur.",
+    oferta: 0,
+    categoria: "bebes",
+  },
+  {
+    id: "6",
+    title: "Hipopotamo",
+    price: 550,
+    pictureUrl: hippo,
+    stock: 5,
+    descripcion: "Lorem ipsum dolor sit amet, consectetur.",
+    oferta: 0,
+    categoria: "peluches",
+  },
+  {
+    id: "7",
+    title: "Mordillo Conejo",
+    price: 300,
+    pictureUrl: bonnyForBabies,
+    stock: 3,
+    descripcion: "Lorem ipsum dolor sit amet, consectetur.",
+    oferta: 0,
+    categoria: "bebes",
+  },
+  {
+    id: "8",
+    title: "Pulpo",
+    price: 550,
+    pictureUrl: octopus,
+    stock: 3,
+    descripcion: "Lorem ipsum dolor sit amet, consectetur.",
+    oferta: 0,
+    categoria: "peluches",
+  },
+  {
+    id: "9",
+    title: "Pulpo",
+    price: 550,
+    pictureUrl: jirafa,
+    stock: 3,
+    descripcion: "Lorem ipsum dolor sit amet, consectetur.",
+    oferta: 0,
+    categoria: "peluches",
+  },
+  {
+    id: "10",
+    title: "Perro",
+    price: 550,
+    pictureUrl: dog,
+    stock: 3,
+    descripcion: "Lorem ipsum dolor sit amet, consectetur.",
+    oferta: 0,
+    categoria: "peluches",
+  },
+  {
+    id: "11",
+    title: "Oso doble",
+    price: 550,
+    pictureUrl: monsterBear,
+    stock: 3,
+    descripcion: "Lorem ipsum dolor sit amet, consectetur.",
+    oferta: 0,
+    categoria: "peluches",
   },
 ];
 
